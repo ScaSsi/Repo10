af8dd5426d4a6311b8e3f4e05ca5c54a9f7bd389
Avujcic1 <avujcic1@etf.unsa.ba>
2022-04-06 23:15:24 +0200

    Adding new properties to object

---
+++ ex06/src/index.js
@@ -0,0 +1,17 @@
+// Create school object here
+var lion = {
+    "name": "Simba",
+    "legs": 4,
+    "tails": 1
+};
+// End of school object
+
+// Create a function below this line
+function myFunction(arg1, arg2) {
+    lion[arg1] = arg2;
+    return lion;
+}
+// End of function
+myFunction("roar", "roar-roar");
+console.log(lion);
+module.exports = myFunction();
