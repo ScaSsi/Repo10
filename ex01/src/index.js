0642540b64bfe9a49c3f9c6af652f2d621a047c4
Avujcic1 <avujcic1@etf.unsa.ba>
2022-04-06 16:12:52 +0200

    Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+var myPet = {
+    species: "Cat",
+    name: "Roki",
+    legs: 4,
+    friends: ["Tik", "Tok"]
+};
+
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
